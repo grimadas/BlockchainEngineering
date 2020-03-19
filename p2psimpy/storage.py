bbc320282064e7f6b2f2a77d3797802d552b3b7a
grimadas <bulat.nasrulin@gmail.com>
2020-03-19 15:14:00 +0100

    Add section 4 for notebook 02. Add notebook 03

--- p2psimpy/storage.py
+++ p2psimpy/storage.py
@@ -4,7 +4,11 @@ class Storage:
 
     def __init__(self):
         self.txs = dict()
+        self.times_seen = dict()
 
     def add(self, tx_id, tx):
         if tx_id not in self.txs:
             self.txs[tx_id] = tx
+            self.times_seen[tx_id] = 1
+        else:
+            self.times_seen[tx_id] += 1
